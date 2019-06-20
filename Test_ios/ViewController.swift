d9f6698f9fbb4992f2d74c0f417715ba691beba8
Udom039 <pokemoner799@gmail.com>
2019-06-20 18:20:59 +0700

    Initial Commit

---
+++ Test_ios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_ios
+//
+//  Created by udom on 20/6/2562 BE.
+//  Copyright © 2562 udom Neakaew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Test_iosTests/Test_iosTests.swift
@@ -0,0 +1,34 @@
+//
+//  Test_iosTests.swift
+//  Test_iosTests
+//
+//  Created by udom on 20/6/2562 BE.
+//  Copyright © 2562 udom Neakaew. All rights reserved.
+//
+
+import XCTest
+@testable import Test_ios
+
+class Test_iosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
