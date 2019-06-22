3fd310fc7b27ed70babe1b2bf3200aface14fc2c
Udom039 <pokemoner799@gmail.com>
2019-06-22 21:22:02 +0700

    change start page

--- Test_ios/AppDelegate.swift
+++ Test_ios/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         window = UIWindow()
-        window?.rootViewController = DashboardViewController()
+        window?.rootViewController = RegistersViewController()
         window?.makeKeyAndVisible()
         return true
     }
